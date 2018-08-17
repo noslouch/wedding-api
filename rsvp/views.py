4093248ad999e6d727763cf4c08fe05817a2046c
Brian Whitton <me@brianwhitton.com>
2018-08-17 15:14:36 -0400

    only allow post and patch for guest viewsets

--- rsvp/views.py
+++ rsvp/views.py
@@ -33,3 +33,4 @@ class InvitationViewSet(viewsets.ModelViewSet):
 class GuestViewSet(viewsets.ModelViewSet):
     queryset = Guest.objects.all()
     serializer_class = GuestSerializer
+    http_method_names = ['post', 'patch']

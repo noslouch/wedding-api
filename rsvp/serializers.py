8c3e258b7efeec3ffb7efd76c9d2df1177c25e9b
Brian Whitton <me@brianwhitton.com>
2018-08-17 14:56:04 -0400

    validate included guests when upating an invitation

--- rsvp/serializers.py
+++ rsvp/serializers.py
@@ -34,3 +34,40 @@ class InvitationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Invitation
         fields = ('id', 'plus_one', 'rehearsal_dinner', 'music_pref', 'guests')
+
+    def validate_guests(self, guests):
+        """
+        validates given guests:
+         - no more than 2 guests per invite
+         - ignore any included values for invitation so guests can't switch invite
+         - ensure all guests have an id
+         - ensure given IDs exist
+         - only guests already associated with an invite are accepted
+         - make sure a guest can't rsvp to the rehearsal dinner if they weren't invited
+        """
+        if len(guests) > 2:
+            raise serializers.ValidationError("Only two guests per invite.")
+
+        invite = get_object_or_404(Invitation, pk=self.instance.pk)
+        valid_guests = invite.guests.values_list('id', flat=True)
+
+        for guest in guests:
+            # disregard given invitation, prevents guests swtching invites
+            guest.pop('invitation', None)
+
+            if not guest.get('id'):
+                raise serializers.ValidationError("All guests must have an id.")
+
+            if not Guest.objects.filter(pk=guest['id']).exists():
+                raise serializers.ValidationError(
+                    "Guest with id {} does not exist in the database.".format(guest['id']))
+
+            if guest['id'] not in valid_guests:
+                raise serializers.ValidationError("Guest does not belong to this invitation.")
+
+            if not invite.rehearsal_dinner:
+                if guest.get('rehearsal_rsvp'):
+                    raise serializers.ValidationError(
+                        "Invitation does not provide for a rehearsal dinner. Please remove 'rehearsal_rsvp' key."
+                    )
+        return guests

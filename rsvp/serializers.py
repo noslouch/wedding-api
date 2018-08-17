c94980f0ff5240d72a3307fa0b0a8f4eb89cd057
Brian Whitton <me@brianwhitton.com>
2018-08-17 14:35:37 -0400

    new guest validation
    when creating a new guest that comes with an invitation, ensure the 
    invitation allows for a plus one

--- rsvp/serializers.py
+++ rsvp/serializers.py
@@ -17,6 +17,14 @@ class GuestSerializer(serializers.ModelSerializer):
             'rehearsal_rsvp',
             'invitation')
 
+    def validate(self, data):
+        if self.context['request'].method == 'POST':
+            invitation = data.get('invitation')
+            # assert that given invitation allows for a plus one
+            if invitation and not invitation.plus_one:
+                raise serializers.ValidationError("Guest cannot be added to given invitation. Invitation does not provide for a plus one.")
+        return super().validate(data)
+
 
 class InvitationSerializer(serializers.ModelSerializer):
     guests = GuestSerializer(many=True)

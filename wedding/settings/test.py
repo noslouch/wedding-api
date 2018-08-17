e018c52d22b92f436f18821f4f6600dabd33f5e6
Brian Whitton <me@brianwhitton.com>
2018-08-17 15:23:55 -0400

    test settings

---
+++ wedding/settings/test.py
@@ -0,0 +1,9 @@
+from .base import *
+
+ALLOWED_HOSTS = ['localhost']
+CORS_ORIGIN_ALLOW_ALL = True
+DEBUG = True
+
+# keep the html browseable api in dev
+from rest_framework import routers
+ROUTER = routers.DefaultRouter(trailing_slash=False)

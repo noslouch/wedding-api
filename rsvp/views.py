d14a806e9abce95461cb245ad5e3b5eb648fece9
Brian Whitton <me@brianwhitton.com>
2018-08-17 15:14:18 -0400

    only allow filter or patch requests
    unless we're in dev

--- rsvp/views.py
+++ rsvp/views.py
@@ -1,11 +1,15 @@
+from django.conf import settings
 from django.db.models import Q
 from rest_framework import viewsets
+from rest_framework.exceptions import ParseError
+
 from .models import Invitation, Guest
 from .serializers import InvitationSerializer, GuestSerializer
 
 
 class InvitationViewSet(viewsets.ModelViewSet):
     serializer_class = InvitationSerializer
+    http_method_names = ['get', 'patch']
 
     def get_queryset(self):
         query = self.request.query_params.get('q')
@@ -18,6 +22,12 @@ class InvitationViewSet(viewsets.ModelViewSet):
             ).distinct()
         else:
             return Invitation.objects.all()
+
+    def list(self, request):
+        if not self.request.query_params.get('q') and not settings.DEBUG:
+            raise ParseError('Must supply a query parameter.')
+        else:
+            return super().list(request)
 
 
 class GuestViewSet(viewsets.ModelViewSet):

ad05b0e5ea41a8a8d2ab7c5b5538fe8c9f46ad28
Brian Whitton <me@brianwhitton.com>
2018-08-16 20:14:36 -0400

    secret key will be passed in during deploys

--- wedding/settings/base.py
+++ wedding/settings/base.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kmh2zb3nqi_2ol^boq!$67-!=qtv^0k&mb449*1oc_p!&n+wb1'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
+                            'kmh2zb3nqi_2ol^boq!$67-!=qtv^0k&mb449*1oc_p!&n+wb1')
 
 
 # Application definition

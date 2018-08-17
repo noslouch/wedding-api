d0d4a9649d4c79244d8d855546b57bd731d9f7ae
Brian Whitton <me@brianwhitton.com>
2018-08-17 15:13:39 -0400

    adds filtering to invite endpoint

--- rsvp/views.py
+++ rsvp/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import viewsets
 from .models import Invitation, Guest
 from .serializers import InvitationSerializer, GuestSerializer
@@ -5,6 +6,18 @@ from .serializers import InvitationSerializer, GuestSerializer
 
 class InvitationViewSet(viewsets.ModelViewSet):
     serializer_class = InvitationSerializer
+
+    def get_queryset(self):
+        query = self.request.query_params.get('q')
+        if query:
+            return Invitation.objects.filter(
+                Q(address__icontains=query) |
+                Q(guests__last_name__icontains=query) |
+                Q(guests__first_name__icontains=query) |
+                Q(guests__email__icontains=query)
+            ).distinct()
+        else:
+            return Invitation.objects.all()
 
 
 class GuestViewSet(viewsets.ModelViewSet):
